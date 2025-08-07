13f96feb1e832cd1b0d9ed93a6adbc04ff10bfb4
Pedro Simonato <pedrohenrryksimonato@gmail.com>
2025-08-07 11:08:29 -0300

    Atualiza o projeto para .NET 9.0 e implementa métodos de instalação de aplicativos nas classes Iphone e Nokia

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama OK
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private string Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -22,5 +27,10 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void EspecificacoesDoModelo()
+        {
+            Console.WriteLine($"Modelo: {Modelo}, IMEI: {IMEI}, Memória: {Memoria}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "123456789012345", memoria: "32GB");
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.EspecificacoesDoModelo();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 13", imei: "987654321098765", memoria: "128GB");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+iphone.EspecificacoesDoModelo();
